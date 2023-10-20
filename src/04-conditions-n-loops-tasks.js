442fc58a4a20773543820058159e30d68732ac52
banshee-dev <nevozmojke@gmail.com>
2023-10-20 20:04:50 +0300

    add solution in conditions-n-loops tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -209,7 +209,7 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'noon' => 'noon'
  */
 function reverseString(str) {
-  return [...str].reverse().join("");
+  return [...str].reverse().join('');
 }
 
 
