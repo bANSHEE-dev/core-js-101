2cd0361c5f1d20c651d6e265468c2e79e5e5eb6c
banshee-dev <nevozmojke@gmail.com>
2023-10-20 18:05:20 +0300

    add solutions in conditions-n-loops tasks

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-// * @example
-// *   'aaaaa' => 5
-// *   'b'     => 1
-// *   ''      => 0
-// */
-function getStringLength(value) {
- return value.length
+// *   12345 => 54321
+// *   1111  => 1111
+// *   87354 => 45378
+// *   34143 => 34143
+// *
+function reverseInteger(num) {
+return num.toString().split("").reverse().join("");
 }
 
-console.log(getStringLength("b"));
+console.log(reverseInteger(12345));
+
+// *   12345 => 54321
+// *   1111  => 1111
+// *   87354 => 45378
+// *   34143 => 34143
+
